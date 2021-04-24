3a09909cb9f4e297eb4176aa063eebc1fb2f1b39
Harshit Srivastava <harshit.srivastava@icloud.com>
2021-04-25 00:11:07 +0530

    updated

--- store/src/main/java/controller/ProductController.java
+++ store/src/main/java/controller/ProductController.java
@@ -3,7 +3,6 @@ package controller;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,5 +14,5 @@ public class ProductController {
 	public @ResponseBody String get(){
 		return "Abhinav";
 	}
-
+	
 }

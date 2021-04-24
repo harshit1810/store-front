a9aa974ba587ea38051e112015ac96dc74fd6034
Harshit Srivastava <harshit.srivastava@icloud.com>
2021-04-24 18:44:55 +0530

    added basic workflows for main and stage branch (#4)

--- store-ui/karma.conf.js
+++ store-ui/karma.conf.js
@@ -36,9 +36,11 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    autoWatch: false,
+    browsers: [
+      'ChromeHeadless'
+    ],
+    singleRun: true,
+    restartOnFileChange: false
   });
 };

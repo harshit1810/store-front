17f5e28179b02fb9e18b362245a57146b8bfc995
Harshit Srivastava <harshit.srivastava@icloud.com>
2021-05-02 13:54:42 +0530

    added categories service

---
+++ demo/src/main/java/com/storebackend/category/util/CategoriesConstants.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.storebackend.category.util;
+
+/**
+ * @author hsriv
+ *
+ */
+public final class CategoriesConstants {
+	public static final String ApiVersion = "v1";
+}

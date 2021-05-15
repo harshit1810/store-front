4f452a13a00a8c5a793e5447a764da5531300c5f
abhinavjoseph <bhjsph@gmail.com>
2021-05-15 15:06:30 +0530

    hotfix: allow empty lifecycle method

--- store-ui/src/app/app.module.ts
+++ store-ui/src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     TopMenuComponent
   ],
   imports: [
@@ -21,6 +19,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
--- store-ui/src/app/login/login.module.ts
+++ store-ui/src/app/login/login.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginPageComponent } from './login-page/login-page.component';
@@ -10,6 +10,7 @@ import { LoginRoutingModule } from './login-routing.module'
   imports: [
     CommonModule,
     LoginRoutingModule
-  ]
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class LoginModule { }
